95f9d5a4159ab68cb24359e09c667e7f3a536d8a
Dmitro Adieliev <adelevdmitriy@gmail.com>
2025-12-18 00:04:09 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import './App.css'
-import Counter from './components/counter'
 
 function App() {
-  return (
-    <>
-      <Counter />
-      <div className="text-3xl font-bold">
-        dqwdwqwdq
-        <button className="btn btn-primary">dwqdqwqqwdqw</button>
-        <button className="btn btn-info">Info</button>
-      </div>
-    </>
-  )
+  return <div></div>
 }
 
 export default App
