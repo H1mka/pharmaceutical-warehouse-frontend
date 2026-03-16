6a7849b186d26f6f6cd109e2f6c0b807cb5bd458
Samira Budzhelida <miraholmes13@gmail.com>
2026-03-16 15:24:36 +0100

    Add reusable table

--- src/views/CustomerView.jsx
+++ src/views/CustomerView.jsx
@@ -1,4 +1,16 @@
+import Table from "../components/Table";
+import mocksMed from "../pages/mockMedications.json";
+
 const CustomerView = () => {
-  return <div> This is Customer page</div>;
+  // const headers = Object.keys(data[0]).map((item) => {
+  //   return { title: item };
+  // });
+
+  return (
+    <div>
+      <h2>This is Customer page</h2>
+      <Table data={mocksMed}></Table>
+    </div>
+  );
 };
 export default CustomerView;
--- src/views/PharmacistView.jsx
+++ src/views/PharmacistView.jsx
@@ -1,4 +1,73 @@
+import { useState, useEffect } from "react";
+import mocksMed from "../pages/mockMedications.json";
+
 const PharmacistView = () => {
-  return <div> This is Pharmacist pages </div>;
+  const [searchingMed, setSearchingMed] = useState("");
+  const [medications, setMedications] = useState([]);
+
+  useEffect(() => {
+    setMedications(mocksMed);
+  }, []);
+
+  let findedMed = medications.filter((item) => {
+    if (searchingMed.length > 0)
+      return item.name.toLowerCase().includes(searchingMed.toLowerCase());
+  });
+
+  return (
+    <div>
+      <h1>Medications</h1>
+      <input
+        className="input validator outline-none border-[#ecf9ff99]"
+        type="search"
+        id="search"
+        placeholder="Search"
+        value={searchingMed}
+        onChange={(e) => setSearchingMed(e.target.value)}
+      />
+      <div>
+        {" "}
+        <h2>Are you looking for this?</h2>
+        {findedMed.map((item) => (
+          <div key={item.id}>
+            <ul>
+              <li>{item.name}</li>
+              <li>{item.category}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Quantity</th>
+            <th>Expiry</th>
+            <th>Location</th>
+            <th>ExpiryDate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mocksMed.map((med) => (
+            <tr key={med.id} className="hover:bg-base-300">
+              <td>{med.name}</td>
+              <td>{med.category}</td>
+              <td>{med.quantity}</td>
+              <td>{med.expiryDate}</td>
+              <td>{med.location}</td>
+              <td>{med.expiryDate}</td>
+              <td>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default PharmacistView;
