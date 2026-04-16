5a55c644f67dc3837d167da4281604cef636010c
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-04-16 18:45:26 +0200

    create reusable TablePagination component

---
+++ src/components/TablePagination.jsx
@@ -0,0 +1,32 @@
+const TablePagination = ({ pagination = {}, fetchData = Function }) => {
+  const { page: currPage, total_pages = 0 } = pagination
+
+  // Check if total pages is number, if not, set one page by default
+  const buttons_pages = typeof total_pages === 'number' ? Array.from({ length: total_pages }, (_, i) => i + 1) : [1]
+
+  const toNextPage = async (nextPage) => {
+    if (typeof nextPage !== 'number') return
+
+    await fetchData({ page: nextPage })
+  }
+
+  return (
+    <div className='table-pagination-wrapper flex justify-center'>
+      <div className='join'>
+        {buttons_pages.map((page) => {
+          return (
+            <button
+              className={`join-item btn btn-md ${currPage === page && 'btn-active'}`}
+              key={page}
+              onClick={() => toNextPage(page)}
+            >
+              {page}
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default TablePagination
