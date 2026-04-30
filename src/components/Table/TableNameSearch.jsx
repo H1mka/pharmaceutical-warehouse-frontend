2588397f4fb7d8146f8f1270bb2ef6e9f501000f
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-04-30 23:19:42 +0200

    create table name search

---
+++ src/components/Table/TableNameSearch.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const TableNameSearch = ({ className = '', fetchData = Function }) => {
+  const [search, setSearch] = useState('')
+
+  const submitSearch = (searchValue = search) => {
+    if (typeof fetchData !== 'function') return ''
+
+    fetchData({ name: searchValue })
+  }
+
+  const handleReset = () => {
+    setSearch('')
+    submitSearch('')
+  }
+
+  return (
+    <div className={`flex gap-2 ${className}`}>
+      <input
+        className='w-full max-w-md rounded-xl border border-gray-300 px-4 py-2.5 text-md shadow-sm outline-none transition-all duration-200 placeholder:text-gray-400 focus:border-(--primary-blue)'
+        type='text'
+        placeholder='Search product...'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button className='btn bg-(--primary-blue)' onClick={submitSearch}>
+        Search
+      </button>
+      <button className='btn bg-(--primary-blue)' onClick={handleReset}>
+        Reset
+      </button>
+    </div>
+  )
+}
+
+export default TableNameSearch
