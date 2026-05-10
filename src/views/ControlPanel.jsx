9c27a19ccdeefea8508175d7eefaf85304b40072
Dmitro Adieliev <adelevdmitriy@gmail.com>
2026-05-10 21:02:57 +0200

    fix auth logic

--- src/views/ControlPanel.jsx
+++ src/views/ControlPanel.jsx
@@ -1,41 +1,48 @@
-import { useState, useEffect } from "react";
-import "../components/ControlPanel.scss";
-import { useManipulator } from "../hooks";
-import { manipulatorApi } from "../api";
-import Table from "../components/Table";
-import TablePagination from "../components/TablePagination";
+import { useState, useEffect } from 'react'
+import '../components/ControlPanel.scss'
+import { useManipulator } from '../hooks'
+import { manipulatorApi } from '../api'
+import Table from '../components/Table/Table'
+import TablePagination from '../components/Table/TablePagination'
 
 const ControlPanel = () => {
   const [systemStatus, setSystemStatus] = useState({
     manipulatorState: 'Unknown',
-    manipulatorLoc: 'Unknown'
+    manipulatorLoc: 'Unknown',
   })
 
-  const { fetchAllLogs, logs, pagination, isLoading } = useManipulator();
+  const { fetchAllLogs, logs, pagination, isLoading } = useManipulator()
 
   useEffect(() => {
-    manipulatorApi.getState()
+    manipulatorApi
+      .getState()
       .then((res) => {
         setSystemStatus({
           manipulatorState: res.data.status || 'Unknown',
-          manipulatorLoc: res.data.position || 'Unknown'
-        });
+          manipulatorLoc: res.data.position || 'Unknown',
+        })
       })
-      .catch((err) => console.error("Помилка при завантаженні статусу маніпулятора:", err));
-  }, []);
+      .catch((err) => console.error('Помилка при завантаженні статусу маніпулятора:', err))
+  }, [])
 
   const startRobot = () => {
-    manipulatorApi.updateState("ON").then(res => {
-      setSystemStatus(prev => ({ ...prev, manipulatorState: res.data.status }));
-      fetchAllLogs();
-    }).catch((err) => console.error(err));
+    manipulatorApi
+      .updateState('ON')
+      .then((res) => {
+        setSystemStatus((prev) => ({ ...prev, manipulatorState: res.data.status }))
+        fetchAllLogs()
+      })
+      .catch((err) => console.error(err))
   }
 
   const stopRobot = () => {
-    manipulatorApi.updateState("OFF").then(res => {
-      setSystemStatus(prev => ({ ...prev, manipulatorState: res.data.status }));
-      fetchAllLogs();
-    }).catch((err) => console.error(err));
+    manipulatorApi
+      .updateState('OFF')
+      .then((res) => {
+        setSystemStatus((prev) => ({ ...prev, manipulatorState: res.data.status }))
+        fetchAllLogs()
+      })
+      .catch((err) => console.error(err))
   }
 
   const tableData = logs.map((log) => ({
@@ -43,10 +50,12 @@ const ControlPanel = () => {
     Time: new Date(log.timestamp).toLocaleTimeString(),
     Action: log.operation_type,
     Status: log.operation_status,
-    "Loc ID": log.storage_location || "-",
-    "Prod ID": log.product || "-",
-    Qty: log.product_quantity || "-"
-  }));
+    'Loc ID': log.storage_location || '-',
+    'Prod ID': log.product || '-',
+    Qty: log.product_quantity || '-',
+  }))
+
+  console.log('Tabledata', tableData)
 
   return (
     <div className='control-panel-container'>
@@ -58,19 +67,13 @@ const ControlPanel = () => {
         <li>Manipulator location: {systemStatus.manipulatorLoc}</li>
       </ul>
 
-      <h2 className="section-title">Control Panel</h2>
-      <div className="control-panel-buttons">
-        <button
-          className="state-button"
-          onClick={startRobot}
-          disabled={systemStatus.manipulatorState === 'ON'}>
+      <h2 className='section-title'>Control Panel</h2>
+      <div className='control-panel-buttons'>
+        <button className='state-button' onClick={startRobot} disabled={systemStatus.manipulatorState === 'ON'}>
           Start Robot
         </button>
 
-        <button
-          className='state-button'
-          onClick={stopRobot}
-          disabled={systemStatus.manipulatorState === 'OFF'}>
+        <button className='state-button' onClick={stopRobot} disabled={systemStatus.manipulatorState === 'OFF'}>
           Stop Robot
         </button>
       </div>
@@ -80,18 +83,12 @@ const ControlPanel = () => {
         <p>Loading logs...</p>
       ) : (
         <>
-          {tableData.length > 0 ? (
-            <Table data={tableData} />
-          ) : (
-            <p>No logs found.</p>
-          )}
-          {pagination.total_pages > 1 && (
-            <TablePagination pagination={pagination} fetchData={fetchAllLogs} />
-          )}
+          {tableData.length > 0 ? <Table data={tableData} /> : <p>No logs found.</p>}
+          {pagination.total_pages > 1 && <TablePagination pagination={pagination} fetchData={fetchAllLogs} />}
         </>
       )}
     </div>
   )
 }
 
-export default ControlPanel;
+export default ControlPanel
